c022750b0b92d5c1c73617edefe47bb7f67a1e6b
Ir786921 <imran.raza.cs24@iilmcet.ac.in>
2025-06-09 23:38:57 +0530

    changes all the url after deploying

--- src/app/page.js
+++ src/app/page.js
@@ -13,19 +13,22 @@ export default function Home() {
   <meta name="keywords" content="URL shortener, link shortener, free link shortener, short link generator, custom URL, QR code generator, link tracker, analytics, bitly alternative, shorten link 2025, trending link tool, free url shortener" />
   <meta name="author" content="YourAppName" />
   <meta name="robots" content="index, follow" />
+    <meta name="url" content="https://link-shortner-theta.vercel.app/" />
+    <link rel="canonical" href="https://link-shortner-theta.vercel.app/" />
+
 
  
   <meta property="og:title" content="Free & Fast URL Shortener | Track and Share Links Easily" />
   <meta property="og:description" content="Create short links with analytics and QR codes. Perfect for sharing on social media or marketing campaigns." />
-  <meta property="og:url" content="https://yourdomain.com" />
+  <meta property="og:url" content="https://link-shortner-theta.vercel.app/" />
   <meta property="og:type" content="website" />
-  <meta property="og:image" content="https://yourdomain.com/og-image.png" />
+  <meta property="og:image" content="https://link-shortner-theta.vercel.app/" />
 
 
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="Free Link Shortener | Shorten, Share & Track Links" />
   <meta name="twitter:description" content="Use our modern URL shortener to create clean links with click tracking, analytics, and QR support." />
-  <meta name="twitter:image" content="https://yourdomain.com/twitter-image.png" />
+  <meta name="twitter:image" content="https://link-shortner-theta.vercel.app/" />
   <meta name="twitter:site" content="@yourapphandle" />
 
 
